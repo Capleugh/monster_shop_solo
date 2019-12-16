2936e1f090f02c9b7f366b37e8e927253c7375a8
Alison Vermeil <alison.minton@gmail.com>
2019-12-16 14:54:54 -0700

    Add sad path test for non-matching passwords
    Co-Authored-By: David Barriga-Gomez <davdg25@gmail.com>
    Co-Authored-By: Harrison Levin <harrison483@gmail.com>
    Co-Authored-By: Carleigh Crockett <capleugh@gmail.com>

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "User registration form" do
-  it "creates a new user" do
+  it "creates a new user when all fields are complete and passwords match" do
     visit '/'
 
     within(:css, 'nav') do
@@ -24,5 +24,34 @@ RSpec.describe "User registration form" do
     expect(current_path).to eq('/profile')
 
     expect(page).to have_content("You are now registered and logged in.")
+
+    user = User.last
+
+    expect(user.name).to eq("Alison Vermeil")
+  end
+
+  it "does not create a new user when all fields are complete and passwords do not match" do
+    visit '/'
+
+    within(:css, 'nav') do
+      click_on "Register"
+    end
+
+    expect(current_path).to eq('/users/register')
+
+    fill_in :name, with: "Alison Vermeil"
+    fill_in :address, with: "123 Main St"
+    fill_in :city, with: "Denver"
+    fill_in :state, with: "CO"
+    fill_in :zip, with: 80516
+    fill_in :email, with: "alison123@gmail.com"
+    fill_in :password, with: "password123"
+    fill_in :password_confirmation, with: "sadface"
+
+    click_button "Create User"
+
+    expect(page).to have_content("Password confirmation doesn't match Password")
+    expect(page).to have_button("Create User")
+    expect(page).to_not have_content("You are now registered and logged in.")
   end
 end
