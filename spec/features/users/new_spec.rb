6fb03918cacefa7c40f6b695f157067b7b845719
Carleigh Crockett <capleugh@gmail.com>
2019-12-16 15:59:46 -0700

    Add test for duplicate email
    Co-authored-by: David Barriga-Gomez <50917634+DavidBarriga-Gomez@users.noreply.github.com>
    Co-authored-by: Alison Vermeil <51250305+mintona@users.noreply.github.com>
    Co-authored-by: Harrison Levin <29346170+hale4029@users.noreply.github.com>

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -85,6 +85,43 @@ RSpec.describe "User registration form" do
     expect(page).to_not have_content("Welcome Alison Vermeil, you are now registered and logged in.")
   end
 
+  it "will not save a user with an email already in the system and displays an error message" do
+    user_1 = User.create!(name: "Alison Vermeil",
+                          address: "123 Main St",
+                          city: "Denver",
+                          state: "CO",
+                          zip: 80516,
+                          email: "alison123@gmail.com",
+                          password: "password123",
+                          password_confirmation: "password123")
+    visit "/"
+
+    click_on "Register"
+
+    fill_in :name, with: "Carl Vermeil"
+    fill_in :address, with: "456 South St"
+    fill_in :city, with: "Boulder"
+    fill_in :state, with: "CO"
+    fill_in :zip, with: 80304
+    fill_in :email, with: "alison123@gmail.com"
+    fill_in :password, with: "password123"
+    fill_in :password_confirmation, with: "password123"
+
+    click_button "Create User"
+
+    expect(page).to have_button("Create User")
+    expect(page).to have_content("Email has already been taken")
+    
+    expect(find_field('Name').value).to eq("Carl Vermeil")
+    expect(find_field('Address').value).to eq("456 South St")
+    expect(find_field('City').value).to eq("Boulder")
+    expect(find_field('State').value).to eq("CO")
+    expect(find_field('Zip').value).to eq(80304)
+    expect(find_field('Email').value).to eq("")
+    expect(find_field('Password').value).to eq("")
+    expect(find_field('Password confirmation').value).to eq("")
+  end
+
   it "keeps a user logged in after registering" do
     visit '/'
 
@@ -109,6 +146,4 @@ RSpec.describe "User registration form" do
 
     expect(page).to have_content("Hello, Alison Vermeil!")
   end
-
-
 end
