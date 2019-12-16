830a7eab8f4b24d707f1a0342e1e02407a8512a5
Alison Vermeil <alison.minton@gmail.com>
2019-12-16 15:13:06 -0700

    Add test for user to stay logged in after registration
    Co-Authored-By: David Barriga-Gomez <davdg25@gmail.com>
    Co-Authored-By: Harrison Levin <harrison483@gmail.com>
    Co-Authored-By: Carleigh Crockett <capleugh@gmail.com>

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -55,8 +55,28 @@ RSpec.describe "User registration form" do
     expect(page).to_not have_content("Welcome Alison Vermeil, you are now registered and logged in.")
   end
 
-  xit "keeps a user logged in after registering" do
+  it "keeps a user logged in after registering" do
+    visit '/'
 
+    within(:css, 'nav') do
+      click_on "Register"
+    end
 
+    expect(current_path).to eq('/users/register')
+
+    fill_in :name, with: "Alison Vermeil"
+    fill_in :address, with: "123 Main St"
+    fill_in :city, with: "Denver"
+    fill_in :state, with: "CO"
+    fill_in :zip, with: 80516
+    fill_in :email, with: "alison123@gmail.com"
+    fill_in :password, with: "password123"
+    fill_in :password_confirmation, with: "password123"
+
+    click_button "Create User"
+
+    expect(current_path).to eq('/profile')
+
+    expect(page).to have_content("Hello, Alison Vermeil!")
   end
 end
