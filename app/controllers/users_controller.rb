ea07f08587a7afa919f5cf79cf5159b8eba754e1
Alison Vermeil <alison.minton@gmail.com>
2019-12-16 15:14:08 -0700

    Added session to user create
    Co-Authored-By: David Barriga-Gomez <davdg25@gmail.com>
    Co-Authored-By: Harrison Levin <harrison483@gmail.com>
    Co-Authored-By: Carleigh Crockett <capleugh@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,18 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(session[:user_id])
   end
 
   def create
     user = User.new(user_params)
 
     if user.save
+      session[:user_id] = user.id
       flash[:success] = "Welcome #{user.name}, you are now registered and logged in."
       redirect_to "/profile"
     else
-      flash[:notice] = user.errors.full_messages.to_sentence
+      flash[:error] = user.errors.full_messages.to_sentence
       render :new
     end
   end
