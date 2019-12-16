ce9dc6b564b9ad4408330ce81662b0c27ebbcd7a
Alison Vermeil <alison.minton@gmail.com>
2019-12-16 13:50:28 -0700

    Add paths for creating new user
    Co-Authored-By: David Barriga-Gomez <davdg25@gmail.com>
    Co-Authored-By: Harrison Levin <harrison483@gmail.com>
    Co-Authored-By: Carleigh Crockett <capleugh@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -34,4 +34,7 @@ Rails.application.routes.draw do
   get "/orders/new", to: "orders#new"
   post "/orders", to: "orders#create"
   get "/orders/:id", to: "orders#show"
+
+  get "/users/register", to: "users#new"
+  post "/users", to: "users#create"
 end
