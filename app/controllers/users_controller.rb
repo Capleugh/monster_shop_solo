fcb74bf3da2b9d9e5ab4d2616d467557f67ca54a
Alison Vermeil <alison.minton@gmail.com>
2019-12-16 14:56:10 -0700

    Add new action and refactor create action
    Co-Authored-By: David Barriga-Gomez <davdg25@gmail.com>
    Co-Authored-By: Harrison Levin <harrison483@gmail.com>
    Co-Authored-By: Carleigh Crockett <capleugh@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,25 @@ class UsersController < ApplicationController
   def new
   end
 
+  def show
+  end
+
   def create
-    User.create(user_params)
-    flash[:success] = "You are now registered and logged in."
-    redirect_to "/profile"
+    user = User.new(user_params)
+
+    if user.save
+      flash[:success] = "You are now registered and logged in."
+      redirect_to "/profile"
+    else
+      flash[:notice] = user.errors.full_messages.to_sentence
+
+      # flash[:notice] = "Your passwords do not match."
+      render :new
+    end
   end
 
   private
     def user_params
-      params.permit(:name, :address, :city, :state, :zip, :email, :password)
+      params.permit(:name, :address, :city, :state, :zip, :email, :password, :password_confirmation)
     end
 end
