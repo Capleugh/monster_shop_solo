ff28fbd763457602871a1f20e37db6c56176f794
David Gomez <davdg25@gmail.com>
2019-12-17 15:08:42 -0700

    Create migration adding defualt role to users and factory bot.
    Co-Authored-By: Alison Vermeil <mintona@users.noreply.github.com>
    Co-Authored-By: Harrison <harrison483@gmail.com>
    Co-Authored-By: Carleigh Crockett <Capleugh@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191216205549) do
+ActiveRecord::Schema.define(version: 20191217215947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20191216205549) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role", default: 0
   end
 
   add_foreign_key "item_orders", "items"
