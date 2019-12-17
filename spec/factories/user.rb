10336fd8b36fb7a71b4ef1cceea3761680aa9e6d
Harrison Levin <harrison483@gmail.com>
2019-12-17 11:45:33 -0700

    added auto increment to email

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     sequence(:city) { |n| "Den #{n}" }
     sequence(:state) { |n| "CO #{n}" }
     sequence(:zip) { |n| "8012#{n}" }
-    sequence(:email) { |n| "johndoe1@gmail.com" }
+    sequence(:email) { |n| "johndoe#{n}@gmail.com " }
     sequence(:password) { |n| "password#{n}" }
     sequence(:password_confirmation) { |n| "password#{n}" }
   end
