b603c3dc338e12daa8ad952e0ae8d0f1fcc76e90
Alison Vermeil <alison.minton@gmail.com>
2019-12-17 20:59:21 -0700

    Added before each blocks and tests for redirecting merchants and admins that are logged in
    Co-Authored-By: David Barriga-Gomez <davdg25@gmail.com>
    Co-Authored-By: Harrison Levin <harrison483@gmail.com>
    Co-Authored-By: Carleigh Crockett <capleugh@gmail.com>

--- spec/features/users/login_spec.rb
+++ spec/features/users/login_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
+  before :each do
+    @user = create(:user, role: 0)
+  end
+
   describe 'As a regular User' do
     it 'can login with valid credentials' do
-      user_1 = create(:user, role: 0)
 
       visit '/'
 
@@ -11,32 +14,35 @@ RSpec.describe 'Logging in', type: :feature do
 
       expect(current_path).to eq('/login')
 
-      fill_in :email, with: user_1.email
-      fill_in :password, with: 'password1'
+      fill_in :email, with: @user.email
+      fill_in :password, with: 'password'
       click_on('Submit')
 
       expect(current_path).to eq('/profile')
-      expect(page).to have_content("Welcome back, #{user_1.name} you are now logged in!")
-      expect(page).to have_content("Hello, #{user_1.name}!")
+      expect(page).to have_content("Welcome back, #{@user.name} you are now logged in!")
+      expect(page).to have_content("Hello, #{@user.name}!")
     end
 
     describe "if I am already logged in and visit the login path" do
       it "I am redirected to my profile page" do
-        user = create(:user, role: 0)
 
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
         visit '/login'
 
         expect(current_path).to eq('/profile')
         expect(page).to have_content("You are already logged in.")
+        expect(page).to have_content("Hello, #{@user.name}!")
       end
     end
   end
 
   describe 'As a Merchant Employee User' do
+    before :each do
+      @merchant_employee = create(:user, role: 1)
+    end
+
     it 'can login with valid credentials' do
-      merchant_employee = create(:user, role: 1)
 
       visit '/'
 
@@ -44,19 +50,35 @@ RSpec.describe 'Logging in', type: :feature do
 
       expect(current_path).to eq('/login')
 
-      fill_in :email, with: merchant_employee.email
-      fill_in :password, with: 'password3'
+      fill_in :email, with: @merchant_employee.email
+      fill_in :password, with: 'password'
       click_on('Submit')
 
       expect(current_path).to eq('/merchant/dashboard')
-      expect(page).to have_content("Welcome back, #{merchant_employee.name} you are now logged in!")
-      expect(page).to have_content("Hello, #{merchant_employee.name}! Welcome to your Merchant Dashboard!")
+      expect(page).to have_content("Welcome back, #{@merchant_employee.name} you are now logged in!")
+      expect(page).to have_content("Hello, #{@merchant_employee.name}! Welcome to your Merchant Dashboard!")
+    end
+
+    describe "if I am already logged in and visit the login path" do
+      it "I am redirected to my merchant dashboard page" do
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant_employee)
+
+        visit '/login'
+
+        expect(current_path).to eq(merchant_dashboard_path)
+        expect(page).to have_content("You are already logged in.")
+        expect(page).to have_content("Hello, #{@merchant_employee.name}! Welcome to your Merchant Dashboard!")
+      end
     end
   end
 
   describe 'As a Merchant Admin User' do
+    before :each do
+      @merchant_admin = create(:user, role: 2)
+    end
+
     it 'can login with valid credentials' do
-      merchant_admin = create(:user, role: 2)
 
       visit '/'
 
@@ -64,33 +86,62 @@ RSpec.describe 'Logging in', type: :feature do
 
       expect(current_path).to eq('/login')
 
-      fill_in :email, with: merchant_admin.email
-      fill_in :password, with: 'password4'
+      fill_in :email, with: @merchant_admin.email
+      fill_in :password, with: 'password'
       click_on('Submit')
 
       expect(current_path).to eq('/merchant/dashboard')
-      expect(page).to have_content("Welcome back, #{merchant_admin.name} you are now logged in!")
-      expect(page).to have_content("Hello, #{merchant_admin.name}! Welcome to your Merchant Dashboard!")
+      expect(page).to have_content("Welcome back, #{@merchant_admin.name} you are now logged in!")
+      expect(page).to have_content("Hello, #{@merchant_admin.name}! Welcome to your Merchant Dashboard!")
+    end
+
+    describe "if I am already logged in and visit the login path" do
+      it "I am redirected to my merchant dashboard page" do
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant_admin)
+
+        visit '/login'
+
+        expect(current_path).to eq(merchant_dashboard_path)
+        expect(page).to have_content("You are already logged in.")
+        expect(page).to have_content("Hello, #{@merchant_admin.name}! Welcome to your Merchant Dashboard!")
+      end
     end
   end
 
   describe 'As an Admin User' do
-    it 'can login with valid credentials' do
-      admin = create(:user, role: 3)
+    before :each do
+      @admin = create(:user, role: 3)
+    end
 
+    it 'can login with valid credentials' do
+      
       visit '/'
 
       click_link('Login')
 
       expect(current_path).to eq('/login')
 
-      fill_in :email, with: admin.email
-      fill_in :password, with: 'password5'
+      fill_in :email, with: @admin.email
+      fill_in :password, with: 'password'
       click_on('Submit')
 
       expect(current_path).to eq('/admin/dashboard')
-      expect(page).to have_content("Welcome back, #{admin.name} you are now logged in!")
-      expect(page).to have_content("Hello, #{admin.name}! Welcome to your Admin Dashboard!")
+      expect(page).to have_content("Welcome back, #{@admin.name} you are now logged in!")
+      expect(page).to have_content("Hello, #{@admin.name}! Welcome to your Admin Dashboard!")
+    end
+
+    describe "if I am already logged in and visit the login path" do
+      it "I am redirected to my admin dashboard page" do
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+
+        visit '/login'
+
+        expect(current_path).to eq(admin_dashboard_path)
+        expect(page).to have_content("You are already logged in.")
+        expect(page).to have_content("Hello, #{@admin.name}! Welcome to your Admin Dashboard!")
+      end
     end
   end
 end
