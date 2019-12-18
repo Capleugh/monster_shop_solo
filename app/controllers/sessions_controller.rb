67d89342be065d83ae627310ab563a84827fdb98
Alison Vermeil <alison.minton@gmail.com>
2019-12-17 21:37:44 -0700

    Refactor actions to use helper methods
    Co-Authored-By: David Barriga-Gomez <davdg25@gmail.com>
    Co-Authored-By: Harrison Levin <harrison483@gmail.com>
    Co-Authored-By: Carleigh Crockett <capleugh@gmail.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     if current_default?
       flash[:error] = "You are already logged in."
       redirect_to(profile_path)
-    elsif current_merchant_employee? || current_merchant_admin?
+    elsif current_merchant?
       flash[:error] = "You are already logged in."
       redirect_to(merchant_dashboard_path)
     elsif current_admin?
@@ -15,21 +15,49 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
 
-    if user.authenticate(params[:password]) && user.default?
-      session[:user_id] = user.id
-      flash[:success] = "Welcome back, #{user.name} you are now logged in!"
+    if authenticated_default_user?(user)
+    # if user.authenticate(params[:password]) && user.default?
+      # session[:user_id] = user.id
+      # flash[:success] = "Welcome back, #{user.name} you are now logged in!"
+      successful_login(user)
       redirect_to '/profile'
-    elsif user.authenticate(params[:password]) && (user.merchant_employee? || user.merchant_admin?)
-      session[:user_id] = user.id
-      flash[:success] = "Welcome back, #{user.name} you are now logged in!"
+    elsif authenticated_merchant?(user)
+    # elsif user.authenticate(params[:password]) && (user.merchant_employee? || user.merchant_admin?)
+      # session[:user_id] = user.id
+      # flash[:success] = "Welcome back, #{user.name} you are now logged in!"
+      successful_login(user)
       redirect_to '/merchant/dashboard'
-    elsif user.authenticate(params[:password]) && user.admin?
-      session[:user_id] = user.id
-      flash[:success] = "Welcome back, #{user.name} you are now logged in!"
+    elsif authenticated_admin?(user)
+    # elsif user.authenticate(params[:password]) && user.admin?
+      # session[:user_id] = user.id
+      # flash[:success] = "Welcome back, #{user.name} you are now logged in!"
+      successful_login(user)
       redirect_to '/admin/dashboard'
     else
       flash[:error] = "Sorry, your credentials are bad."
       render :new
     end
   end
+
+  private
+    def current_merchant?
+      current_merchant_employee? || current_merchant_admin?
+    end
+
+    def authenticated_default_user?(user)
+      user.authenticate(params[:password]) && user.default?
+    end
+
+    def authenticated_merchant?(user)
+      user.authenticate(params[:password]) && (user.merchant_employee? || user.merchant_admin?)
+    end
+
+    def authenticated_admin?(user)
+      user.authenticate(params[:password]) && user.admin?
+    end
+
+    def successful_login(user)
+      session[:user_id] = user.id
+      flash[:success] = "Welcome back, #{user.name} you are now logged in!"
+    end
 end
