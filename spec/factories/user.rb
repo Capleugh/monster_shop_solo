ec4449f73da271ad39273424d5017c71547e6225
Alison Vermeil <alison.minton@gmail.com>
2019-12-18 14:53:08 -0700

    Remove extra space in email string that was causing problems in tests
    Co-Authored-By: David Barriga-Gomez <davdg25@gmail.com>
    Co-Authored-By: Harrison Levin <harrison483@gmail.com>
    Co-Authored-By: Carleigh Crockett <capleugh@gmail.com>

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     sequence(:city) { |n| "Den #{n}" }
     sequence(:state) { |n| "CO #{n}" }
     sequence(:zip) { |n| "8012#{n}" }
-    sequence(:email) { |n| "johndoe#{n}@gmail.com " }
+    sequence(:email) { |n| "johndoe#{n}@gmail.com" }
     password {"password"}
     password_confirmation {"password"}
     role {0}
