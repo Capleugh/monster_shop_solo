2749a15e8f06c1d2b9bb1d02c030bee282c6755f
Alison Vermeil <alison.minton@gmail.com>
2019-12-18 11:19:58 -0700

    Removed comments
    Co-Authored-By: David Barriga-Gomez <davdg25@gmail.com>
    Co-Authored-By: Harrison Levin <harrison483@gmail.com>
    Co-Authored-By: Carleigh Crockett <capleugh@gmail.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,21 +16,12 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
 
     if authenticated_default_user?(user)
-    # if user.authenticate(params[:password]) && user.default?
-      # session[:user_id] = user.id
-      # flash[:success] = "Welcome back, #{user.name} you are now logged in!"
       successful_login(user)
       redirect_to '/profile'
     elsif authenticated_merchant?(user)
-    # elsif user.authenticate(params[:password]) && (user.merchant_employee? || user.merchant_admin?)
-      # session[:user_id] = user.id
-      # flash[:success] = "Welcome back, #{user.name} you are now logged in!"
       successful_login(user)
       redirect_to '/merchant/dashboard'
     elsif authenticated_admin?(user)
-    # elsif user.authenticate(params[:password]) && user.admin?
-      # session[:user_id] = user.id
-      # flash[:success] = "Welcome back, #{user.name} you are now logged in!"
       successful_login(user)
       redirect_to '/admin/dashboard'
     else
