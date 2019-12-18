505161923a1cdca46c742310d86a3274826eb048
David Gomez <davdg25@gmail.com>
2019-12-18 15:10:00 -0700

    Add all current user types to application helper methods
    Co-Authored-By: Carleigh Crockett <Capleugh@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :cart, :current_user, :current_default?
+  helper_method :cart, :current_user, :current_default?, :current_merchant_employee?, :current_merchant_admin?, :current_admin?
 
   def cart
     @cart ||= Cart.new(session[:cart] ||= Hash.new(0))
