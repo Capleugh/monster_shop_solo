b58629d82024212d360fd9613e273b3c1e3c2115
Harrison Levin <harrison483@gmail.com>
2019-12-17 21:40:22 -0700

    made some changes to insure the loggin is valid and then also updated so the passowrd is always correct

--- spec/features/users/logout_spec.rb
+++ spec/features/users/logout_spec.rb
@@ -1,17 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe 'Logging in', type: :feature do
+RSpec.describe 'Log Out', type: :feature do
   describe 'As a regular User' do
     it 'can login with valid credentials' do
       mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
       paper = mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 25)
-      user_1 = create(:user, role: 0)
-
+      user = create(:user, role: 0)
+      
       visit '/'
       click_link('Login')
-      fill_in :email, with: user_1.email
-      fill_in :password, with: 'password1'
+      fill_in :email, with: user.email
+      fill_in :password, with: "password#{user.name.split.last.to_i}"
       click_on('Submit')
+      expect(current_path).to eq('/profile')
+      expect(page).to have_content("Welcome back, #{user.name} you are now logged in!")
+      expect(page).to have_content("Hello, #{user.name}!")
       visit "/items/#{paper.id}"
       click_on "Add To Cart"
       expect(page).to have_content("Cart: 1")
