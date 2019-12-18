57a5a8bf05edd6cd32f44f2164e5a44feac58c31
Alison Vermeil <alison.minton@gmail.com>
2019-12-17 20:18:28 -0700

    Refactor users show to use current_user method
    Co-Authored-By: David Barriga-Gomez <davdg25@gmail.com>
    Co-Authored-By: Harrison Levin <harrison483@gmail.com>
    Co-Authored-By: Carleigh Crockett <capleugh@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(session[:user_id])
+    # @user = User.find(session[:user_id])
+    @user = current_user
   end
 
   def create
