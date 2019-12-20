63fbe1c4670381ad3188d4cdb56b2fe6760d5493
Harrison Levin <harrison483@gmail.com>
2019-12-19 21:41:44 -0700

    story 25 test

--- spec/features/cart/show_spec.rb
+++ spec/features/cart/show_spec.rb
@@ -103,5 +103,24 @@ RSpec.describe 'Cart show' do
         end
         expect(page).to_not have_css("#cart-item-#{paper.id}")
       end
+    describe "show page notifies visitor to log in" do
+      it "if not logged in, says you must be logged in to checkout" do
+        mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+        meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+
+        tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 10)
+        paper = mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
+        pencil = mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+        visit "/items/#{paper.id}"
+        click_on "Add To Cart"
+        visit "/items/#{tire.id}"
+        click_on "Add To Cart"
+        visit "/items/#{pencil.id}"
+        click_on "Add To Cart"
+        visit '/cart'
+
+        expect(page).to have_content("You must register or log in to checkout.")
+      end
+    end
   end
 end
