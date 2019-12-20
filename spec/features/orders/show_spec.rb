3290b89bd6ce4d40d8667fb50081ad0fd7010d06
Harrison Levin <harrison483@gmail.com>
2019-12-20 12:48:04 -0700

    added thumbnail to show page
    Co-Authored-By: Carleigh Crockett <Capleugh@users.noreply.github.com>

--- spec/features/orders/show_spec.rb
+++ spec/features/orders/show_spec.rb
@@ -11,17 +11,17 @@ RSpec.describe("Order Show Page") do
       @order.item_orders.create(item: @item_3, quantity: 10, price: @item_3.price)
     end
 
-    xit "routes to order show page" do
+    it "routes to order show page" do
       visit "profile/orders"
       expect(page).to have_content(@user.orders.first.name)
     end
 
     it "route to order show page and have order details" do
-      #visit "profile/orders"
-      #click_on "#{@order.id}"
-      #expect(current_path).to eq("/profile/orders/#{@order.id}")
-      visit "/profile/orders/#{@order.id}"
-      
+      visit "profile/orders"
+      click_on "#{@order.id}"
+      expect(current_path).to eq("/profile/orders/#{@order.id}")
+      #visit "/profile/orders/#{@order.id}"
+
       expect(page).to have_content("#{@order.id}")
       expect(page).to have_content("#{@order.created_at}")
       expect(page).to have_content("#{@order.updated_at}")
@@ -33,7 +33,7 @@ RSpec.describe("Order Show Page") do
         expect(page).to have_content("10")
         expect(page).to have_content("#{@item_1.price}")
         expect(page).to have_content(@item_1.price * 10)
-        #expect(page).to have_css("img[src*='#{@item_1.image}']")
+        expect(page).to have_css("img[src*='#{@item_1.image}']")
       end
 
       within "#item-#{@item_2.id}" do
@@ -42,7 +42,7 @@ RSpec.describe("Order Show Page") do
         expect(page).to have_content("10")
         expect(page).to have_content("#{@item_2.price}")
         expect(page).to have_content(@item_2.price * 10)
-        #expect(page).to have_css("img[src*='#{@item_2.image}']")
+        expect(page).to have_css("img[src*='#{@item_2.image}']")
       end
 
       within "#item-#{@item_3.id}" do
@@ -51,7 +51,7 @@ RSpec.describe("Order Show Page") do
         expect(page).to have_content("10")
         expect(page).to have_content("#{@item_3.price}")
         expect(page).to have_content(@item_3.price * 10)
-        #expect(page).to have_css("img[src*='#{@item_3.image}']")
+        expect(page).to have_css("img[src*='#{@item_3.image}']")
       end
 
       expect(page).to have_content((@item_3.price * 10) + (@item_2.price * 10) + (@item_1.price * 10))
