de542c12e091dbb068b0540af4d77a0cc3eae1c7
Harrison Levin <harrison483@gmail.com>
2019-12-19 21:27:45 -0700

    cart model tests

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -15,35 +15,85 @@ RSpec.describe Cart do
     end
 
     it 'total_items' do
-
+      cart = Cart.new(Hash.new(0))
+      cart.add_item(@paper)
+      cart.add_item(@pencil)
+      result = cart.total_items
+      expect(result).to eq(2)
     end
 
     it 'items' do
-
+      cart = Cart.new(Hash.new(0))
+      cart.add_item(@paper.id.to_s)
+      cart.add_item(@pencil.id.to_s)
+      result = cart.items
+      expect(result.length).to eq(2)
     end
 
     it 'subtotal(item)' do
-
+      cart = Cart.new(Hash.new(0))
+      cart.add_item(@paper.id.to_s)
+      cart.add_item(@pencil.id.to_s)
+      cart.add_item(@pencil.id.to_s)
+      result = cart.subtotal(@pencil)
+      expect(result).to eq(4)
     end
 
     it 'total' do
-
+      cart = Cart.new(Hash.new(0))
+      cart.add_item(@paper.id.to_s)
+      cart.add_item(@pencil.id.to_s)
+      cart.add_item(@pencil.id.to_s)
+      result = cart.total
+      expect(result).to eq(24)
     end
 
     it 'add_quantity(item_id)' do
-
+      cart = Cart.new(Hash.new(0))
+      cart.add_item(@paper.id.to_s)
+      cart.add_item(@pencil.id.to_s)
+      cart.add_item(@pencil.id.to_s)
+      result = cart.add_quantity(@pencil.id.to_s)
+      expect(result).to eq(3)
     end
 
     it 'subtract_quantity(item_id)' do
-
+      cart = Cart.new(Hash.new(0))
+      cart.add_item(@paper.id.to_s)
+      cart.add_item(@pencil.id.to_s)
+      cart.add_item(@pencil.id.to_s)
+      result = cart.subtract_quantity(@pencil.id.to_s)
+      expect(result).to eq(1)
     end
 
     it 'limit_reached?(item_id)' do
-
+      pencil_1 = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 2)
+      cart = Cart.new(Hash.new(0))
+      cart.add_item(@paper.id.to_s)
+      cart.add_item(@pencil.id.to_s)
+      cart.add_item(pencil_1.id.to_s)
+      result = cart.limit_reached?(@pencil.id.to_s)
+      expect(result).to eq(false)
+      result = cart.limit_reached?(pencil_1.id.to_s)
+      expect(result).to eq(false)
+      cart.add_item(pencil_1.id.to_s)
+      result = cart.limit_reached?(pencil_1.id.to_s)
+      expect(result).to eq(true)
     end
 
     it 'quantity_zero?(item_id)' do
-
+      pencil_1 = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 2)
+      cart = Cart.new(Hash.new(0))
+      cart.add_item(pencil_1.id.to_s)
+      cart.add_item(pencil_1.id.to_s)
+      result = cart.quantity_zero?(pencil_1.id.to_s)
+      expect(result).to eq(false)
+      cart.subtract_quantity(pencil_1.id.to_s)
+      result = cart.quantity_zero?(pencil_1.id.to_s)
+      expect(result).to eq(false)
+      cart.subtract_quantity( pencil_1.id.to_s)
+      result = cart.quantity_zero?(pencil_1.id.to_s)
+      expect(result).to eq(true)
     end
   end
 end
