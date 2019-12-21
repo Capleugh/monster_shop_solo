84e7db90ae7d470908c95c39e8330ea6a3c02c53
Alison Vermeil <alison.minton@gmail.com>
2019-12-21 13:01:33 -0700

    Remove comments
    Co-Authored-By: Carleigh Crockett <47065120+Capleugh@users.noreply.github.com>

--- spec/features/orders/index_spec.rb
+++ spec/features/orders/index_spec.rb
@@ -42,22 +42,7 @@ RSpec.describe 'As A User', type: :feature do
       expect(page).to have_content(@order.updated_at)
       expect(page).to have_content(@order.status)
       expect(page).to have_content(@order.items.count)
-      # expect(page).to have_content(480)
       expect(page).to have_content(@order.grandtotal)
     end
   end
 end
-
-
-
-# User Story 28, User Profile displays Orders
-#
-# As a registered user
-# When I visit my Profile Orders page, "/profile/orders"
-# I see every order I've made, which includes the following information:
-# - the ID of the order, which is a link to the order show page
-# - the date the order was made
-# - the date the order was last updated
-# - the current status of the order
-# - the total quantity of items in the order
-# - the grand total of all items for that order
