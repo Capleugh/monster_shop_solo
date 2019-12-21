f7fee571fb06addb8845999536dba8421d5a7d39
Carleigh Crockett <capleugh@gmail.com>
2019-12-20 22:20:08 -0700

    Update merchant_dashboard_path to merchant_path

--- spec/features/merchant/merchant_dashboard_spec.rb
+++ spec/features/merchant/merchant_dashboard_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "As a merchant employee or merchant admin" do
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_employee)
 
-      visit merchant_dashboard_path
+      visit merchant_path
       # save_and_open_page
       expect(page).to have_content(bike_shop.name)
       expect(page).to have_content(bike_shop.address)
@@ -18,21 +18,23 @@ RSpec.describe "As a merchant employee or merchant admin" do
     end
   end
 
-  describe "when I visit my merchant dashboard" do
-    it "I see the name and full address of the merchant I work for" do
-      bike_shop = create(:merchant)
-      merchant_admin = create(:user, role: 2, merchant: bike_shop)
+  it "I see the name and full address of the merchant I work for" do
+    bike_shop = create(:merchant)
+    merchant_admin = create(:user, role: 2, merchant: bike_shop)
 
 
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_admin)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_admin)
 
-      visit merchant_dashboard_path
+    visit merchant_path
 
-      expect(page).to have_content(bike_shop.name)
-      expect(page).to have_content(bike_shop.address)
-      expect(page).to have_content(bike_shop.city)
-      expect(page).to have_content(bike_shop.state)
-      expect(page).to have_content(bike_shop.zip)
-    end
+    expect(page).to have_content(bike_shop.name)
+    expect(page).to have_content(bike_shop.address)
+    expect(page).to have_content(bike_shop.city)
+    expect(page).to have_content(bike_shop.state)
+    expect(page).to have_content(bike_shop.zip)
+  end
+
+  it "I see a link to view my own items and when I click that link, my URI route should be '/merchant/items'" do
+
   end
 end
