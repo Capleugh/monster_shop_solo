3e2a5bff87457716fbf98cd3221bad1b8448cf00
Carleigh Crockett <capleugh@gmail.com>
2019-12-22 14:00:55 -0700

    Create migration to change default status of an order from 0 to 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191220221254) do
+ActiveRecord::Schema.define(version: 20191222202551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 20191220221254) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.integer "status", default: 0
+    t.integer "status", default: 1
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
