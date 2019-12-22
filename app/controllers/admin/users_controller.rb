84ea27f65b3580c52d1a3c0c97a395374ed439da
Carleigh Crockett <capleugh@gmail.com>
2019-12-22 02:03:06 -0700

    Add show action to admin users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,11 @@
 class Admin::UsersController < Admin::BaseController
 
   def index
+  end
 
-  end  
+  def show
+    @user = User.find(params[:id])
+
+    redirect_to(admin_user_path)
+  end
 end
