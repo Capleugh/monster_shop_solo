b53be5fa251fd4cf2f4b69e869e5f7d3a7853843
Carleigh Crockett <capleugh@gmail.com>
2019-12-22 02:01:10 -0700

    Add show action to resources users within admin

--- config/routes.rb
+++ config/routes.rb
@@ -60,7 +60,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get '/', to: 'dashboard#index'
-    resources :users, only: [:index]
+    resources :users, only: [:index, :show]
     resources :merchants, only: [:index, :show]
   end
 end
