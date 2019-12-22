fc100e3e2d971a63e29223c5ed113bc80ea82c12
Carleigh Crockett <capleugh@gmail.com>
2019-12-21 21:26:05 -0700

    Delete duplicate merchant_dashboard_spec file

--- spec/features/merchant/dashboard_spec.rb
+++ spec/features/merchant/dashboard_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe "As a merchant employee or merchant admin" do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_employee)
 
       visit merchant_path
-      
+
       expect(page).to have_content(bike_shop.name)
       expect(page).to have_content(bike_shop.address)
       expect(page).to have_content(bike_shop.city)
@@ -37,7 +37,7 @@ RSpec.describe "As a merchant employee or merchant admin" do
   it "I see a link to view my own items and when I click that link, my URI route should be '/merchant/items'" do
     bike_shop = create(:merchant)
     merchant_employee = create(:user, role: 1, merchant: bike_shop)
-    # do we actually want to test that items appear on page? User story only suggests that we create a path
+
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_employee)
 
 
@@ -58,5 +58,54 @@ RSpec.describe "As a merchant employee or merchant admin" do
     click_link 'View Your Items'
 
     expect(current_path).to eq(merchant_items_path)
+  end
+
+  describe "if any users have pending orders containing items I sell" do
+    it "lists those orders and information about it" do
+      @order = create(:order)
+      @order_2 = create(:order, created_at: Date.today)
+      @user = User.last
+      @item_1 = create(:item)
+      @item_2 = create(:item)
+      @item_3 = create(:item)
+
+      merchant_3 = @item_1.merchant
+      # merchant_4 = @item_2.merchant
+      # merchant_5 = @item_3.merchant
+
+      merchant_3_employee = create(:user, role: 1, merchant: merchant_3)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_3_employee)
+
+      @order.item_orders.create(item: @item_1, quantity: 10, price: @item_1.price)
+      @order.item_orders.create(item: @item_2, quantity: 10, price: @item_2.price)
+      @order.item_orders.create(item: @item_3, quantity: 10, price: @item_3.price)
+
+      @order_2.item_orders.create(item: @item_1, quantity: 5, price: @item_1.price)
+      @order_2.item_orders.create(item: @item_2, quantity: 5, price: @item_2.price)
+
+      @order_2.update(status: "cancelled")
+
+      visit merchant_path
+      within "#pending-orders" do
+        within "#order-#{@order.id}" do
+          expect(page).to have_content(@order.created_at)
+          expect(page).to have_content(@order.items.count)
+          expect(page).to have_content(@order.grandtotal)
+          click_link("Order Number: #{@order.id}")
+        end
+      end
+
+      expect(current_path).to eq("/merchant/orders/#{@order.id}")
+
+      visit merchant_path
+
+      within "#pending-orders" do
+        expect(page).to_not have_link("Order Number: #{@order_2.id}")
+        expect(page).to_not have_content(@order_2.created_at)
+        expect(page).to_not have_content("Number of Items: #{@order_2.items.count}")
+        expect(page).to_not have_content(@order_2.grandtotal)
+      end
+    end
   end
 end
