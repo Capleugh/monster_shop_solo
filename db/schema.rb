75d44021fa094790b9b3a7a189c01490a27cb1d5
Carleigh Crockett <capleugh@gmail.com>
2020-01-06 15:14:04 -0700

    Update merchants table to include enabled column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191222202551) do
+ActiveRecord::Schema.define(version: 20200106220146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20191222202551) do
     t.integer "zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "enabled?", default: true
   end
 
   create_table "orders", force: :cascade do |t|
