cb460ec31cf3222a0738ff546ac87bd505bab17b
Carleigh Crockett <capleugh@gmail.com>
2020-01-06 16:12:27 -0700

    Create update action in admin merchants controller

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -6,4 +6,24 @@ class Admin::MerchantsController < Admin::BaseController
   def show
     @merchant = Merchant.find(params[:id])
   end
+
+  def update
+    merchant = Merchant.find(params[:id])
+    change_status_of_merchant(merchant)
+
+    redirect_to admin_merchants_path
+  end
+
+  private
+
+    def merchant_params
+      params.permit(:name, :address, :city, :state, :zip, :enabled?)
+    end
+
+    def change_status_of_merchant(merchant)
+      merchant.update(enabled?: false)
+
+      flash[:notice] = "#{merchant.name} is now disabled."
+      # redirect_to admin_merchants_path
+    end
 end
