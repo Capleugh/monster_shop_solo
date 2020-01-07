9099a65f0c2e9a4eb2c9de19ad82373bf70a3c2f
Carleigh Crockett <capleugh@gmail.com>
2020-01-07 00:15:13 -0700

    Comment out update action as it isnt getting hit likely a problem with routes

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,4 +2,34 @@ class Admin::ItemsController < Admin::BaseController
   def index
     @merchant = Merchant.find(params[:merchant_id])
   end
+  #
+  # def update
+  #   require "pry"; binding.pry
+  #   merchant = Merchant.find(params[:merchant_id])
+  #   item = merchant.items.find(params[:id])
+  #
+  #   if deactivate?
+  #     deactivate(item)
+  #   elsif activate?
+  #     activate(item)
+  #   end
+  #   # redirect_to admin_merchants_path
+  # end
+  #
+  #   private
+  #     def deactivate?
+  #       params[:status] == "deactivate"
+  #     end
+  #
+  #     def deactivate(item)
+  #       item.toggle!(:active?)
+  #     end
+  #
+  #     def activate?
+  #       params[:status] == "activate"
+  #     end
+  #
+  #     def activate(item)
+  #       item.toggle!(:active?)
+  #     end
 end
