fa0152dfaa715d67d98651caa383d8e2649aa606
Harrison Levin <harrison483@gmail.com>
2020-01-06 19:11:27 -0700

    merchant_show_order controller

--- app/controllers/merchant/orders_controller.rb
+++ app/controllers/merchant/orders_controller.rb
@@ -1,5 +1,8 @@
+require './app/models/poros/merchant_show'
+
 class Merchant::OrdersController < Merchant::BaseController
   def show
-
-  end 
+    order = Order.find(params[:id])
+    @merchant_show = MerchantShow.new(order)
+  end
 end
