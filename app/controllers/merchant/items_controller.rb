15b14f6bec89823a7cce97abec0cd0f66dfa670f
Alison Vermeil <alison.minton@gmail.com>
2020-01-06 20:42:53 -0700

    Add activate button and update controller action accordingly for merchant items

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -8,10 +8,34 @@ class Merchant::ItemsController < Merchant::BaseController
 
   def update
     item = Item.find(params[:id])
-    if item.toggle!(:active?)
-      flash[:success] = "#{item.name} is no longer for sale."
+
+    if deactivate?
+      deactivate(item)
+    elsif activate?
+      activate(item)
     end
     
     redirect_to merchant_items_path
   end
+
+  private
+    def deactivate?
+      params[:status] == "deactivate"
+    end
+
+    def deactivate(item)
+      if item.toggle!(:active?)
+        flash[:success] = "#{item.name} is no longer for sale."
+      end
+    end
+
+    def activate?
+      params[:status] == "activate"
+    end
+
+    def activate(item)
+      if item.toggle!(:active?)
+        flash[:success] = "#{item.name} is available for sale."
+      end
+    end
 end
