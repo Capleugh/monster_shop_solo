a3a7b8b13b7e70765c447a8329204ab19d459436
Alison Vermeil <alison.minton@gmail.com>
2020-01-06 19:37:50 -0700

    Remove unnecessary private methods from merchant dashboard controller whos actions are covered in the base controller

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -3,12 +3,4 @@ class Merchant::DashboardController < Merchant::BaseController
   def show
   end
 
-  private
-    def require_not_admin
-      render file: '/public/404'unless !current_admin?
-    end
-
-    def require_not_default
-      render file: "/public/404" unless !current_default?
-    end
 end
