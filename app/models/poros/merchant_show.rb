865a146f8f47e816678dc731fce7e57111f6cc11
Harrison Levin <harrison483@gmail.com>
2020-01-06 20:46:17 -0700

    poro update to include the find quantity method

--- app/models/poros/merchant_show.rb
+++ app/models/poros/merchant_show.rb
@@ -1,12 +1,17 @@
 class MerchantShow
-  attr_reader :order
+  attr_reader :order, :merchant
 
-  def initialize(order)
+  def initialize(order, merchant)
     @order = order
+    @merchant = merchant
   end
 
   def find_items
-    @order.items.joins(:item_orders).where("item_orders.order_id = #{@order.id}")
+    @order.items.where("items.merchant_id = #{@merchant.merchant_id}")
+  end
+
+  def find_quantity(item_id)
+    ItemOrder.where(item_id: item_id).where(order_id: @order.id).pluck(:quantity).first
   end
 
   def find_customer
