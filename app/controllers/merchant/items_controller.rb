978ea790ffc757ea1aa14d5505032ce26d37d402
David Gomez <davdg25@gmail.com>
2020-01-08 15:19:36 -0700

    Refactor update and create actions

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -12,9 +12,7 @@ class Merchant::ItemsController < Merchant::BaseController
     @merchant = Merchant.find(current_user.merchant_id)
     @item = @merchant.items.create(item_params)
     if @item.save
-      # binding.pry
       if params[:item][:image] == ""
-      # if params.require(:item).permit(:image)[:image] == ""
         @item.update(image: default_image)
       end
       flash[:success] = "Item added!"
@@ -33,43 +31,13 @@ class Merchant::ItemsController < Merchant::BaseController
     @merchant = Merchant.find(current_user.merchant_id)
   end
 
-  # def status_update
-  #   item = Item.find(params[:id])
-  #
-  #   if deactivate?
-  #     deactivate(item)
-  #   elsif activate?
-  #     activate(item)
-  #   end
-  #
-  #   redirect_to merchant_items_path
-  # end
-
   def update
     if params[:status].nil?
-    #if there is no params[:status] (aka params[:status].nil?), proceed with all the code below.
       @item = Item.find(params[:format])
-      @item.update(item_params)
-      if @item.save
-        if params[:item][:image] == ""
-          @item.update(image: default_image)
-        end
-        redirect_to "/merchant/items"
-        flash[:success] = 'Item Has Been Updated'
-      else
-        flash[:error] = @item.errors.full_messages.to_sentence
-        redirect_back(fallback_location: "/merchant/items")
-      end
+      update_item_info(@item)
     else
       item = Item.find(params[:id])
-
-      if deactivate?
-        deactivate(item)
-      elsif activate?
-        activate(item)
-      end
-
-      redirect_to merchant_items_path
+      update_status(item)
     end
   end
 
@@ -108,5 +76,28 @@ class Merchant::ItemsController < Merchant::BaseController
 
     def default_image
       'https://scontent-den4-1.cdninstagram.com/v/t51.2885-15/e35/11375785_1097843546897273_287775595_n.jpg?_nc_ht=scontent-den4-1.cdninstagram.com&_nc_cat=105&_nc_ohc=yrczfty57n0AX-7OByN&oh=d6298df08426babd3eb105ea14b12329&oe=5E9B3359'
+    end
+
+    def update_status(item)
+      if deactivate?
+        deactivate(item)
+      elsif activate?
+        activate(item)
+      end
+
+      redirect_to merchant_items_path
+    end
+
+    def update_item_info(item)
+      if item.update(item_params)
+        if params[:item][:image] == ""
+          item.update(image: default_image)
+        end
+        redirect_to "/merchant/items"
+        flash[:success] = 'Item Has Been Updated'
+      else
+        flash[:error] = item.errors.full_messages.to_sentence
+        redirect_back(fallback_location: "/merchant/items")
+      end
     end
 end
