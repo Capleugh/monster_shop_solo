3fd73ac1c82bb735ec45c177528cfb9cd5e807e4
Harrison Levin <harrison483@gmail.com>
2020-01-07 20:53:27 -0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -63,7 +63,7 @@ Rails.application.routes.draw do
     get '/', to: 'dashboard#index'
     resources :users, only: [:index, :show]
     resources :merchants, only: [:index, :show, :update] do
-      resources :items, only: [:index]
+      resources :items, only: [:index, :new, :create]
     end
   end
 end
