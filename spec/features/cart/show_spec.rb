2f51d1dfd019aa16031a110250b34d5030284c91
David Gomez <davdg25@gmail.com>
2020-01-08 16:32:24 -0700

    Add links to flash message when checking out if not a user. update tests
    Co-Authored-By: Alison Vermeil <mintona@users.noreply.github.com>
    Co-Authored-By: Carleigh Crockett <Capleugh@users.noreply.github.com>

--- spec/features/cart/show_spec.rb
+++ spec/features/cart/show_spec.rb
@@ -103,6 +103,7 @@ RSpec.describe 'Cart show' do
         end
         expect(page).to_not have_css("#cart-item-#{paper.id}")
       end
+
     describe "show page notifies visitor to log in" do
       it "if not logged in, says you must be logged in to checkout" do
         mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
@@ -111,6 +112,7 @@ RSpec.describe 'Cart show' do
         tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 10)
         paper = mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
         pencil = mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+
         visit "/items/#{paper.id}"
         click_on "Add To Cart"
         visit "/items/#{tire.id}"
@@ -118,8 +120,17 @@ RSpec.describe 'Cart show' do
         visit "/items/#{pencil.id}"
         click_on "Add To Cart"
         visit '/cart'
-        #expect(page).to_not have_link("Checkout")
+        expect(page).to_not have_link("Checkout")
         expect(page).to have_content("You must register or log in to checkout.")
+        expect(page).to have_link('register')
+        expect(page).to have_link('log in')
+
+        click_on('register')
+        expect(current_path).to eq('/users/register')
+
+        click_on('Cart')
+        click_on('log in')
+        expect(current_path).to eq('/login')
       end
     end
   end
