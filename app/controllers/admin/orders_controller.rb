0e9acc968d934907eec35898dd4fada4ee9db7a5
Carleigh Crockett <capleugh@gmail.com>
2020-01-09 15:34:37 -0700

    Add destroy action to admin orders controller

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,36 @@
 class Admin::OrdersController < Admin::BaseController
 
+  def index
+    @user = current_user
+  end
+
   def show
     @order = Order.find(params[:id])
   end
+
+  def destroy
+    order = Order.find(params[:id])
+    if order.status == 'pending'
+      order.item_orders.change_status_to_unfulfilled
+      reset_inventory(order)
+      change_order_status_to_cancelled(order)
+    else
+      flash[:error] = "Unable to cancel order."
+      redirect_back(fallback_location: admin_path)
+    end
+  end
+
+  private
+    def change_order_status_to_cancelled(order)
+      order.update(status: 3)
+      flash[:success] = "#{order.id} has been cancelled."
+      redirect_to admin_path
+    end
+
+    def reset_inventory(order)
+      order.items.each do |item|
+        quantity = item.quantity_ordered(order)
+        item.increase_item_inventory(quantity)
+      end
+    end
 end
