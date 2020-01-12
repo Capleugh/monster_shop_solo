555502109b331ec0d29dc160ee2bd41eff7442da
Carleigh Crockett <capleugh@gmail.com>
2020-01-12 04:23:13 -0700

    Create and test validations for coupons

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,3 +1,8 @@
 class Coupon < ApplicationRecord
   belongs_to :merchant
+
+  validates :name, uniqueness: true, presence: true
+  validates :code, uniqueness: true, presence: true
+
+  validates_numericality_of :percent, less_than_or_equal_to: 1
 end
--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 describe Coupon, type: :model do
+  describe "validations" do
+    it {should validate_uniqueness_of :name}
+    it {should validate_uniqueness_of :code}
+    it {should validate_numericality_of(:percent).is_less_than_or_equal_to(1)}
+  end
+
   describe "relationships" do
     it { should belong_to :merchant }
   end
