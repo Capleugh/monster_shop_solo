827d58ea0b1251e9eb71ba1470d3e5e736cf4360
Carleigh Crockett <capleugh@gmail.com>
2020-01-12 21:29:47 -0700

    Add greater than 0 validation to coupon test

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 describe Coupon, type: :model do
   describe "validations" do
-    it {should validate_uniqueness_of :name}
-    it {should validate_uniqueness_of :code}
-    it {should validate_numericality_of(:percent).is_less_than_or_equal_to(1)}
+    it { should validate_uniqueness_of :name }
+    it { should validate_uniqueness_of :code }
+
+    it { should validate_numericality_of(:percent).is_less_than_or_equal_to(1) }
+    it { should validate_numericality_of(:percent).is_greater_than(0) }
   end
 
   describe "relationships" do
