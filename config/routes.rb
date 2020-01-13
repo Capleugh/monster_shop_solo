086725787432ea0956ea541322fd1969b048d349
Carleigh Crockett <capleugh@gmail.com>
2020-01-13 00:12:40 -0700

    Add update path to routes

--- config/routes.rb
+++ config/routes.rb
@@ -57,7 +57,7 @@ Rails.application.routes.draw do
     get '/', to: 'dashboard#show'
     resources :orders, only: [:show, :update]
     resources :items, only: [:index, :show, :update, :destroy, :new, :create, :edit]
-    resources :coupons, only: [:index, :show, :new, :create, :edit]
+    resources :coupons, only: [:index, :show, :new, :create, :edit, :update]
   end
 
   namespace :admin do
