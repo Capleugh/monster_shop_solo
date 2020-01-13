be12fe32567aacd69070e7751b40ee66bc0d0559
Carleigh Crockett <capleugh@gmail.com>
2020-01-13 00:10:13 -0700

    Change user role in test

--- spec/features/merchant/coupon_show_spec.rb
+++ spec/features/merchant/coupon_show_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe "As a merchant employee or admin"  do
+RSpec.describe "As a merchant employee"  do
   describe "when I visit a coupons show page" do
     it "I see all of that coupon's info and a button to edit a coupon" do
       bike_shop = create(:merchant)
-      merchant_admin = create(:user, role: 2, merchant: bike_shop)
+      merchant_employee = create(:user, role: 1, merchant: bike_shop)
       coupon_1 = bike_shop.coupons.create(name: "25% weekend promo", code: "WKD25", percent: 0.25)
       coupon_2 = bike_shop.coupons.create(name: "50% labor day promo", code: "LABOR50", percent: 0.5)
 
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_admin)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_employee)
 
       visit "/merchant/coupons/#{coupon_1.id}"
 
